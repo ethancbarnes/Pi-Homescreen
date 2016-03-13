0dcd3efb1772c6f523d705bce9f15b40706de2c0
ethancbarnes <ethancbarnes@gmail.com>
2016-03-13 13:28:52 -0400

    removed client id

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 // Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
 
-var CLIENT_ID = '476135981645-saemggldq22gk9ov8prim5bstplg4hlt.apps.googleusercontent.com';
+var CLIENT_ID = 'CLIENT-ID-HERE';
 
 var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
